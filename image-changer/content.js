d652e69864ebdc978f8e2ac547969a347c25657c
axt-one <rwatanabe1623@gmail.com>
2022-06-18 18:50:00 +0900

    modify image-changer to use images in the extension directory

--- image-changer/content.js
+++ image-changer/content.js
@@ -1,15 +1,15 @@
-const imgUrl = [
-    'https://1.bp.blogspot.com/-BtA-5lUEZ7U/XdttdTmBVZI/AAAAAAABWIc/WDbvWBJkN3UzdKhCAXkRU5h3TGhJwE16ACNcBGAsYHQ/s400/fruit_slice_grapefruit.png',
-    'https://1.bp.blogspot.com/-A5z35AuVSwA/XdttdxDvwwI/AAAAAAABWIg/Qxl_6sg1v6Asa0AQEEOLg6fe_wEB8bCDQCNcBGAsYHQ/s400/fruit_slice_grapefruit_pink.png'
+const imgFiles = [
+    'resources/fruit_slice_grapefruit_pink.png',
+    'resources/fruit_slice_grapefruit.png'
 ];
 
 function replaceImage() {
     const elements = document.querySelectorAll('img');
-    let i;
     for (const e of elements) {
-        i = Math.floor(Math.random() * imgUrl.length);
-        e.src = imgUrl[i];
-        e.srcset = imgUrl[i] + ' 1x,' + imgUrl[i] + ' 2x';
+        let i = Math.floor(Math.random() * imgFiles.length);
+        let imgURL = chrome.runtime.getURL(imgFiles[i]);
+        e.src = imgURL;
+        e.srcset = imgURL + ' 1x,' + imgURL + ' 2x';
     }
 }
 
