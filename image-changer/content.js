a0b6e9c30aa2746a073b1f63a906921151682306
axt-one <rwatanabe1623@gmail.com>
2022-06-18 17:06:14 +0900

    adapt to change of elements

--- image-changer/content.js
+++ image-changer/content.js
@@ -2,7 +2,8 @@ const imgUrl = [
     'https://1.bp.blogspot.com/-BtA-5lUEZ7U/XdttdTmBVZI/AAAAAAABWIc/WDbvWBJkN3UzdKhCAXkRU5h3TGhJwE16ACNcBGAsYHQ/s400/fruit_slice_grapefruit.png',
     'https://1.bp.blogspot.com/-A5z35AuVSwA/XdttdxDvwwI/AAAAAAABWIg/Qxl_6sg1v6Asa0AQEEOLg6fe_wEB8bCDQCNcBGAsYHQ/s400/fruit_slice_grapefruit_pink.png'
 ];
-window.onload = () => {
+
+function replaceImage() {
     const elements = document.querySelectorAll('img');
     let i;
     for (const e of elements) {
@@ -11,3 +12,13 @@ window.onload = () => {
         e.srcset = imgUrl[i] + ' 1x,' + imgUrl[i] + ' 2x';
     }
 }
+
+window.addEventListener('load', replaceImage);
+
+const observer = new MutationObserver(replaceImage);
+
+observer.observe(document, {
+    subtree: true,
+    characterData: true,
+    childList: true
+});
